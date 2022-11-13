48b754775565a9d161bed5e5a8cb7151147e2042
John Constant <johnconstantis@gmail.com>
2022-11-13 17:09:02 +0000

    Add summernote urls and CategoryAdmin class

--- category/admin.py
+++ category/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Category, Recipe, Ingredient, Instruction
+from django_summernote import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
-admin.site.register(Instruction)
+
+
+@admin.register(Category)
+class CategoryAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
