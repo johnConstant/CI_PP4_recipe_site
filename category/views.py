68dbd976a7935e089f1f1d9e6601048df73a8b6a
John Constant <johnconstantis@gmail.com>
2022-11-13 20:06:13 +0000

    Add CategoryList View

--- category/views.py
+++ category/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from django.http import HttpResponseRedirect
+from .models import Category
 
-# Create your views here.
+
+class CategoryList(generic.ListView):
+    model = Category
+    queryset = Category.objects.filter(status=1).order_by('-created_date')
+    template_name = 'index.html'
+    paginate_by = 6
