d2caae6a16fa8da6b51c6701f77eeb470f68c33a
John Constant <johnconstantis@gmail.com>
2022-11-13 19:49:12 +0000

    Add enhanced admin features to the CategoryAdmin class

--- category/admin.py
+++ category/admin.py
@@ -8,6 +8,10 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Category)
 class CategoryAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = ({'slug': ('title',)})
+    list_filter = ('created_date', 'last_modified')
+    list_display = ('title', 'created_date')
+    search_fields = ['title', 'description']
     summernote_fields = ('description')
 
 
