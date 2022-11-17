94d0118c318d6ae9a26bdfd85b26a36a08ffda0c
John Constant <johnconstantis@gmail.com>
2022-11-17 21:33:09 +0000

    Add all recipe list view

--- recipe_maker/views.py
+++ recipe_maker/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.views import generic, View
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.template.defaultfilters import slugify
+# from .forms import CategoryForm
+from .models import Recipe
 
-# Create your views here.
+
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_date')
+    template_name = 'recipes.html'
+    paginate_by = 12
