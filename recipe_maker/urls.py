b12b3e1bb1fce5db6eba47da37d3cba399a8b174
John Constant <johnconstantis@gmail.com>
2022-11-20 16:01:50 +0000

    Add recipe/add url to urls.py

--- recipe_maker/urls.py
+++ recipe_maker/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='recipes'),
-    # path(
-    #     'add/', views.CategoryAdd.as_view(), name='add_category'
-    #     ),
+    path(
+        'add/', views.AddRecipe.as_view(), name='add_recipe'
+        ),
     # path(
     #     'edit/<slug>', views.CategoryUpdate.as_view(), name='update_category'
     #     ),
