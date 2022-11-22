0f524f3516b577e744c0b0dc71b8636f1c7a4837
John Constant <johnconstantis@gmail.com>
2022-11-22 22:17:46 +0000

    Add Search Results url to urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home')
+    path('', views.HomePage.as_view(), name='home'),
+    path("search/", views.SearchResults.as_view(), name="search_results"),
 ]
