3854c5930635559d243d5be65567a95506893507
John Constant <johnconstantis@gmail.com>
2022-11-29 21:49:05 +0000

    Add front-end CRUD functions

---
+++ articles/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Article, Comment
+
+
+class ArticleForm(forms.ModelForm):
+    """
+    A class view creating the Article form
+    """
+    class Meta:
+        model = Article
+        fields = ['title', 'topic', 'excerpt', 'content', 'featured_image', 'status']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body', )
